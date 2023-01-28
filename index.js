fc345af1f5d053b2f251391a7b5733b22dfa8dbf
AllenLiou-java <75025074+AllenLiou-java@users.noreply.github.com>
2023-01-28 18:12:20 +0800

    add frontend

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ app.get('/', (req, res) => {
 
 app.use('/gsheet', require('./routes/gsheet'));
 
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+    // Static folder - 此時即可提供在public資料夾下的所有檔案url
+    app.use(express.static(__dirname + '/public/'));
+
+    // Handle SPA
+    // res.sendFile() 可以向瀏覽器發送 靜態頁面；
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 const port = process.env.PORT || 1337;
 
 app.listen(port, () => {

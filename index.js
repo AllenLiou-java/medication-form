234fe99de1d9abe52b3b2bf4b3182fc4df2529e5
AllenLiou-java <75025074+AllenLiou-java@users.noreply.github.com>
2023-02-02 09:54:18 +0800

    update

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ app.use(cors());
 app.use('/api/gsheet', require('./routes/gsheet'));
 
 // Handle production
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     // Static folder - 此時即可提供在public資料夾下的所有檔案url
     app.use(express.static(__dirname + '/public/'));
 
     // Handle SPA
     // res.sendFile() 可以向瀏覽器發送 靜態頁面；
     app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
-// }
+}
 
 const port = process.env.PORT || 1337;
 
